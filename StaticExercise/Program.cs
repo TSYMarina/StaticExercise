afed157e9c14e2010d7b0147d91ba29a088b64ad
Marina Tsymlianskaia <marinatsy16@gmail.com>
2022-02-23 21:03:14 -0700

    * .gitignore: first commit * StaticExercise.sln: first commit * StaticExercise/Program.cs: first commit * StaticExercise/TempConverter.cs: first commit * StaticExercise/StaticExercise.csproj: first commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there, Human! Looking for help with temperature conversions? I can do that for you!\n");
+
+            var celsius = TempConverter.FahrenheitToCelsius(2); 
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(-10); 
+
+            Console.WriteLine("Please see the results of the conversions:\n");
+            Console.WriteLine($"Your input in Fahrenheit equals to {celsius} degrees in Celsius.");
+            Console.WriteLine($"Your input in Celsius equals to  {fahrenheit} degrees in Fahrenheit.");
+
+        }
+
+
+        //static public int ConversionDirection(string userPrompt)
+        //{
+        //    int userInt = 0;
+        //    bool validUserInt = false;
+        //    do
+        //    {
+        //        if (userInt == 1 || userInt == 2)
+        //        {
+        //            validUserInt = true;
+        //            Console.WriteLine("Thanks!\n");
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Sorry, this is not a valid input. You need to choose 1 or 2, retry please:  ");
+        //        }
+        //    }
+        //    while (!validUserInt);
+        //    return userInt;
+        //}
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double CelsiusToFahrenheit(double dInCelsius) 
+        {
+            return (dInCelsius * 9 / 5) + 32;
+        }
+
+        public static double FahrenheitToCelsius(double dInFahrenheit)
+        {
+            return (dInFahrenheit - 32) /1.8;
+        }
+
+        public static double GetTemp(string userPrompt)
+        {
+            double userInput = 0;
+            bool validUserInput = false;
+            do
+            {
+                if (Double.TryParse(Console.ReadLine(), out userInput))
+                {
+                    validUserInput = true;
+                    Console.WriteLine("Thanks!\n");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, this is not a valid input. You need to provide temperature as a double, retry please:  ");
+                }
+            }
+            while (!validUserInput);
+            return userInput;
+        }
+    }
+}
